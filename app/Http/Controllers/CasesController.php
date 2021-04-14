aff095c6f2e9dd568c8f423068e8ec12372cd6ef
nawing2t <naveenkumar@g2techsoft.com>
2021-04-14 22:56:12 +0530

    Initial Commit

---
+++ app/Http/Controllers/CasesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cases;
+use GuzzleHttp\Client;
+
+class CasesController extends Controller
+{
+	public function getCases(){
+		$client = new Client([
+			'base_uri' => 'https://api.coronatracker.com',
+		]);
+
+		$response = $client->request('GET', '/v5/analytics/newcases/country', [
+			'query' => [
+				'countryCode' => 'IN',
+				'startDate' => '2020-01-01',
+				'endDate' => '2021-04-13',
+			]
+		]);
+
+		$body = $response->getBody();
+		$arr_body = json_decode($body);
+		foreach($arr_body as $single_case){
+			
+			$data = new Cases();
+			$data->country_code = $single_case->country_code;
+			$data->affected = $single_case->new_infections;
+			$data->death = $single_case->new_deaths;
+			$data->recovered = $single_case->new_recovered;
+			$data->last_updated = date('Y-m-d', strtotime($single_case->last_updated));
+			$data->save();			
+		}
+		return response()->json('Successfully added');
+		
+	}
+	
+	public function index(){
+		
+	}
+}
